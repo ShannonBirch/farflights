e26d8c672cb4ee908b46d385384782daf71ec62c
gezgblack <shamilataev96@gmail.com>
2016-02-06 22:34:14 +0000

    reverted changes, still playing with rebase

--- files/index.php
+++ files/index.php
@@ -1,6 +1,40 @@
 <html>
+<<<<<<< HEAD
 
 Gonna work on this today hopefully
 
 
 </html>
+=======
+	<head>
+		<title> Far Flights </title>
+		<meta name = "viewport" content = "width=device-width, initial-scale = 1.0">
+		<link href = "css/bootstrap.min.css" rel = "stylesheet">
+		<link href = "css/styles.css" rel = "stylesheet">
+		<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</head>
+	
+	<body>
+	
+		<div class = "navbar navbar-inverse navbar-static-top">
+			<div class = "container">
+				
+					<a href="#" class="navbar-brand">Far Flights</a>
+					
+					
+					
+						<ul class = "nav navbar-nav navbar-right">
+							<li><a href = "#"> Home </a></li>
+							<li><a href = "#"> About </a></li>
+							<li><a href = "#"> Contact </a><li>
+							<li><a href = "#"> Log in </a></li>
+						</ul>
+					
+			</div>
+			
+		</div>	
+	
+	</body>
+</html>	
+>>>>>>> a10281e... Trying to make this webpage responsive
